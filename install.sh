31396467270ace6b9291be11b5755ea7a55b28a9
JiHaD SeR <85656190+J-I-H-A-D@users.noreply.github.com>
2021-07-30 07:52:27 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+pkg update -y
+pkg upgrade -y
+pkg install wget -y
+pkg install ffmpeg -y
+pkg install nodejs -y
+pkg install npm -y
+pkg install tesseract -y
+apt install nmap -y
+npm i
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+npm audit fix
+
+echo "[*] SUBSCRIBER MY YOUTUBE CHANNEL JID MODS YAH/ NAH KETIK AJA node index / npm start"
+
+#!/usr/bin/bash
+
+figlet MechaBot system
+echo -e "> Please Type Enter To Continue"
+read enter
+clear
+echo -e "\033[1;33mTHIS SCRIPT IS FREE, JUST AMEL ASKING FOR HELP\nDON'T FORGET TO SUBSCRIBE YA\n CHANNEL YOUTUBE JID MODS\033[0m\n"
+echo -e "> enter to next"
+read enter
+clear
+node index
